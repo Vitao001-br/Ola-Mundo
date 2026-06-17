19ee13707b2d938ed1890860c4d6818fd1261a8f
Vitao001-br <vitor100oce@gmail.com>
2026-06-17 10:26:18 -0300

    criei um código básico de python

---
+++ codigo-exemplo/main.py
@@ -0,0 +1,10 @@
+print('Olá mundo,como está ?')
+print('1-significa= Bem')
+print('2-significa= Não estou bem hoje!')
+r=int(input('Sua resposta:'))
+if r == 1:
+    print('Que bom que está bem!')
+elif r == 2:
+    print('Melhoras para voçê!')
+else:
+    print('Tente novamente!')
